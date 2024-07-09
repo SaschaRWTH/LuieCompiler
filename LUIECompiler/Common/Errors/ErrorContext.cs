625bc4f610d28c6e86ad7f1b1c6530d58d0c5151
Sascha Thiemann <saschathiemann00@gmail.com>
2024-07-09 14:15:16 +0200

    add error context struct

---
+++ LUIECompiler/Common/Errors/ErrorContext.cs
@@ -0,0 +1,22 @@
+using Antlr4.Runtime;
+
+namespace LUIECompiler.Common.Errors{
+    public struct ErrorContext
+    {
+        public int Line { get; set; }
+        
+        public int Column { get; set; }
+
+        public ErrorContext(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public ErrorContext(IToken token)
+        {
+            Line = token.Line;
+            Column = token.Column;
+        }
+    }
+}
