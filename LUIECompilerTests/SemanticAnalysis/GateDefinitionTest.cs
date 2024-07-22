3b2bc599730f8b931b4b4100ec3f61fa153a1e66
Sascha Thiemann <saschathiemann00@gmail.com>
2024-07-22 22:59:54 +0200

    test: use of undefined gate and woring use of gate

--- LUIECompilerTests/SemanticAnalysis/GateDefinitionTest.cs
+++ LUIECompilerTests/SemanticAnalysis/GateDefinitionTest.cs
@@ -18,7 +18,7 @@ namespace LUIECompilerTests.SemanticAnalysis
             "x c;\n" +
             "swap b, c;";
 
-        public const string UseOfUndefined =
+        public const string UseOfUndefinedParameter =
             "gate swap(a, b) do\n" +
             "    cx a, b;\n" +
             "    cx c, a;\n" +
@@ -29,6 +29,29 @@ namespace LUIECompilerTests.SemanticAnalysis
             "qubit c;\n" +
             "x c;\n" +
             "swap b, c;";
+
+        public const string UseOfUndefinedGate =
+            "gate swap2(a, b) do\n" +
+            "    cx a, b;\n" +
+            "    cx b, a;\n" +
+            "    cx a, b;\n" +
+            "end\n" +
+            "\n" +
+            "qubit b;\n" +
+            "qubit c;\n" +
+            "x c;\n" +
+            "swap b, c;";
+
+        public const string WrongUseOfGateIdentifier =
+            "gate swap(a, b) do\n" +
+            "    cx a, b;\n" +
+            "    cx b, a;\n" +
+            "    cx a, b;\n" +
+            "end\n" +
+            "\n" +
+            "qubit[sizeof(swap)] b;\n" +
+            "qubit c;\n" +
+            "x swap;";
 
         [TestMethod]
         public void SimpleGateDefinitionTest()
@@ -51,15 +74,14 @@ namespace LUIECompilerTests.SemanticAnalysis
             walker.Walk(analysis, parser.parse());
             var error = analysis.Error;
 
-            Console.WriteLine(error.ToString());
-
             Assert.IsFalse(error.ContainsCriticalError);
         }
+
         [TestMethod]
-        public void UseOfUndefinedTest()
+        public void UseOfUndefinedParameterTest()
         {
             var walker = Utils.GetWalker();
-            var parser = Utils.GetParser(UseOfUndefined);
+            var parser = Utils.GetParser(UseOfUndefinedParameter);
             var analysis = new DeclarationAnalysisListener();
             walker.Walk(analysis, parser.parse());
             var error = analysis.Error;
@@ -68,6 +90,37 @@ namespace LUIECompilerTests.SemanticAnalysis
 
             Assert.IsTrue(error.Errors.Count == 1);
             Assert.IsTrue(error.CriticalErrors.Exists(e => e.ErrorContext.Line == 3));
+        }
+
+        [TestMethod]
+        public void UseOfUndefinedGateTest()
+        {
+            var walker = Utils.GetWalker();
+            var parser = Utils.GetParser(UseOfUndefinedGate);
+            var analysis = new DeclarationAnalysisListener();
+            walker.Walk(analysis, parser.parse());
+            var error = analysis.Error;
+
+            Assert.IsTrue(error.ContainsCriticalError);
+
+            Assert.IsTrue(error.Errors.Count == 1);
+            Assert.IsTrue(error.CriticalErrors.Exists(e => e.ErrorContext.Line == 10));
+        }
+
+        [TestMethod]
+        public void WrongUseOfGateIdentifierTest()
+        {
+            var walker = Utils.GetWalker();
+            var parser = Utils.GetParser(WrongUseOfGateIdentifier);
+            var analysis = new TypeCheckListener();
+            walker.Walk(analysis, parser.parse());
+            var error = analysis.Error;
+
+            Assert.IsTrue(error.ContainsCriticalError);
+
+            Assert.IsTrue(error.CriticalErrors.Exists(e => e.ErrorContext.Line == 7));
+            // TODO: "A critical Error occured at (9, 0)" occures twice
+            Assert.IsTrue(error.CriticalErrors.Exists(e => e.ErrorContext.Line == 9));
         }
     }
 }
