510ec667a9690fe8887f862f142283979cf86e4b
Sascha Thiemann <saschathiemann00@gmail.com>
2024-08-04 00:29:45 +0200

    add: qft exmple code, test: qft gate translation

--- LUIECompilerTests/CodeGeneration/GeneralTest.cs
+++ LUIECompilerTests/CodeGeneration/GeneralTest.cs
@@ -4,7 +4,7 @@ using LUIECompiler.Common.Symbols;
 namespace LUIECompilerTests.CodeGeneration;
 
 [TestClass, TestCategory("CodeGeneration")]
-public class BasicTest
+public class GeneralTest
 {
     public const string SimpleInput =
         "qubit c;\n" +
@@ -37,6 +37,41 @@ public class BasicTest
     public const string ParameterizedGateTranslation = 
         "qubit id0;\n" +
         "p(pi * 0.125) id0;\n";
+
+    public const string QFTGate =
+        @"
+            gate qft(reg) do
+                for i in range(sizeof(reg)) do
+                    h reg[i];
+                    for j in range(sizeof(reg) - (i + 1)) do
+                        qif reg[j + (i + 1)] do
+                            p(1/(power(2, (j + 1)))) reg[i];
+                        end
+                    end
+                end
+            end
+            qubit[5] a;
+            qft a;
+        ";
+    public const string QFTGateTranslation =
+        "qubit[5] id0;\n" +
+        "h id0[0];\n" +
+        "ctrl(1) @ p(pi * 0.5) id0[1], id0[0];\n" +
+        "ctrl(1) @ p(pi * 0.25) id0[2], id0[0];\n" +
+        "ctrl(1) @ p(pi * 0.125) id0[3], id0[0];\n" +
+        "ctrl(1) @ p(pi * 0.0625) id0[4], id0[0];\n" +
+        "h id0[1];\n" +
+        "ctrl(1) @ p(pi * 0.5) id0[2], id0[1];\n" +
+        "ctrl(1) @ p(pi * 0.25) id0[3], id0[1];\n" +
+        "ctrl(1) @ p(pi * 0.125) id0[4], id0[1];\n" +
+        "h id0[2];\n" +
+        "ctrl(1) @ p(pi * 0.5) id0[3], id0[2];\n" +
+        "ctrl(1) @ p(pi * 0.25) id0[4], id0[2];\n" +
+        "h id0[3];\n" +
+        "ctrl(1) @ p(pi * 0.5) id0[4], id0[3];\n" +
+        "h id0[4];\n";
+        
+
 
     /// <summary>
     /// Tests the code generation for the simple input.
@@ -72,5 +107,23 @@ public class BasicTest
         Assert.IsNotNull(code);
 
         Assert.AreEqual(ParameterizedGateTranslation, code);
+    }
+
+    /// <summary>
+    /// Tests the code generation for a parameterized gate.
+    /// </summary>
+    [TestMethod]
+    public void QFTGateTest()
+    {
+        var walker = Utils.GetWalker();
+        var parser = Utils.GetParser(QFTGate);
+
+        var codegen = new CodeGenerationListener();
+        walker.Walk(codegen, parser.parse());
+
+        string? code = codegen.CodeGen.GenerateCode()?.ToString();
+        Assert.IsNotNull(code);
+
+        Assert.AreEqual(QFTGateTranslation, code);
     }
 }
