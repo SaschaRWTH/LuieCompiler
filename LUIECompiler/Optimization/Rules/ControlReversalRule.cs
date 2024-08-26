b314960dc5f7a5344984652e1c3ec7ef485bb847
Sascha Thiemann <saschathiemann00@gmail.com>
2024-08-26 14:21:14 +0200

    add: control reversal application and documentation

--- LUIECompiler/Optimization/Rules/ControlReversalRule.cs
+++ LUIECompiler/Optimization/Rules/ControlReversalRule.cs
@@ -1,4 +1,5 @@
 using LUIECompiler.CodeGeneration.Codes;
+using LUIECompiler.CodeGeneration.Exceptions;
 using LUIECompiler.Common;
 using LUIECompiler.Optimization.Graphs;
 using LUIECompiler.Optimization.Graphs.Nodes;
@@ -11,11 +12,78 @@ namespace LUIECompiler.Optimization.Rules
 
         public static readonly ControlReversalRule Rule = new();
 
+        /// <summary>
+        /// Saves the H gates to be removed when applying the rule.
+        /// </summary>
         protected GateNode[] HGates { get; private set; } = [];
 
         public override void Apply(WirePath path)
         {
-            throw new NotImplementedException();
+            if (path.Length != 1)
+            {
+                throw new InternalException()
+                {
+                    Reason = "The length of the path was not 1."
+                };
+            }
+
+            if (path.Start is not GateNode node)
+            {
+                throw new InternalException()
+                {
+                    Reason = "The rule can only be applied to gate nodes."
+                };
+            }
+
+            if (node.Gate == GateType.CX)
+            {
+                if (node.GateCode.Parameters.Count != 2)
+                {
+                    throw new InternalException()
+                    {
+                        Reason = $"The gate {node.GateCode} has an invalid number of parameters.",
+                    };
+                }
+
+                QubitCode guard = node.GateCode.Parameters[0];
+                QubitCode target = node.GateCode.Parameters[1];
+
+                node.GateCode.Parameters[0] = target;
+                node.GateCode.Parameters[1] = guard;
+            }
+
+            if (node.Gate == GateType.X)
+            {
+                if (node.GateCode.Parameters.Count != 1)
+                {
+                    throw new InternalException()
+                    {
+                        Reason = $"The gate {node.GateCode} has an invalid number of parameters.",
+                    };
+                }
+                if (node.GateCode.Guards.Count != 1)
+                {
+                    throw new InternalException()
+                    {
+                        Reason = $"The gate {node.GateCode} has an invalid number of guards for the rule application.",
+                    };
+                }
+
+                GuardCode guard = node.GateCode.Guards[0];
+                QubitCode target = node.GateCode.Parameters[0];
+
+                node.GateCode.Parameters[0] = guard.Qubit;
+                node.GateCode.Guards[0] = new()
+                {
+                    Qubit = target,
+                    Negated = false,
+                };
+            }
+
+            foreach (GateNode hNode in HGates)
+            {
+                hNode.Remove();
+            }
         }
 
         public override bool IsApplicable(WirePath path)
@@ -44,6 +112,11 @@ namespace LUIECompiler.Optimization.Rules
             return false;
         }
 
+        /// <summary>
+        /// Checks if the rule is applicable to a cx gate.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         private bool IsApplicableCX(GateNode node)
         {
             if (node.GateCode.Guards.Count != 0)
@@ -63,6 +136,11 @@ namespace LUIECompiler.Optimization.Rules
             return IsApplicableQubits(node, guard, target);
         }
 
+        /// <summary>
+        ///  Checks if the rule is applicable to a guarded x gate.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         private bool IsApplicableGuardedX(GateNode node)
         {
             HashSet<GraphGuard> guards = node.GetGuardQubits();
@@ -87,6 +165,14 @@ namespace LUIECompiler.Optimization.Rules
             return IsApplicableQubits(node, guard.Qubit, parameters[0]);
         }
 
+        /// <summary>
+        /// Checks if the rule is applicable to the <paramref name="node"/> with the given
+        /// <paramref name="guard"/> and <paramref name="target"/>. 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="guard"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
         private bool IsApplicableQubits(GateNode node, GraphQubit guard, GraphQubit target)
         {
             if (node.GetPredecessor(guard) is not GateNode guardPredecessor || !IsSingleQubitHGate(guardPredecessor))
@@ -122,6 +208,11 @@ namespace LUIECompiler.Optimization.Rules
             return true;
         }
 
+        /// <summary>
+        /// Indicates whether a given <paramref name="node"/> is a single qubit H gate.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         private bool IsSingleQubitHGate(GateNode node)
         {
             return node.Gate == GateType.H && node.GateCode.Guards.Count == 0;
