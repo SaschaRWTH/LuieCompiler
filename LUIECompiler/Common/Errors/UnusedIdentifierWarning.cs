63e9e1f1691dca7806b38c6195b790f1db86c02e
SaschaRWTH <saschathiemann00@gmail.com>
2024-09-12 12:57:51 +0200

    add: unused identifier warning

---
+++ LUIECompiler/Common/Errors/UnusedIdentifierWarning.cs
@@ -0,0 +1,24 @@
+using LUIECompiler.Common.Symbols;
+
+namespace LUIECompiler.Common.Errors
+{
+    public class UsedIdendifierWarning : CompilationError
+    {
+        public Symbol Symbol { get; }
+
+        /// <summary>
+        /// Creates a new invalid range warning.
+        /// </summary>
+        /// <param name="context">Context where the range was created.</param>
+        /// <param name="start">Start value of the range.</param>
+        /// <param name="end">End value of the range.</param>
+        public UsedIdendifierWarning(ErrorContext context, Symbol symbol)
+        {
+            Type = ErrorType.Warning;
+            ErrorContext = context;
+            Symbol = symbol;
+            Description = $"The {Symbol.GetType()} '{Symbol.Identifier}' was defined but never used.";
+        }
+
+    }
+}
